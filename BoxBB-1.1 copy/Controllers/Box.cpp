7019216bc8746cad1502e799e957902fa0e46fc1
aah233 <aah233@inlumine.ual.com>
2024-02-19 23:18:02 +0100

    Actualizo

---
+++ BoxBB-1.1 copy/Controllers/Box.cpp
@@ -0,0 +1,40 @@
+#include "box.hpp"
+#include "utils.h"
+
+template<typename F, typename... Ts>
+class box : public BOX<F, Ts...> {
+    itV F;
+    
+public:
+    // Implementación de la función virtual
+    BOX<F, Ts...> compareTo(const BOX<F, Ts...>& T1, const BOX<F, Ts...>& T2) const override {
+        return BOX<F, Ts...>();
+    }
+
+
+    /**
+     * @brief Get the new NBox value.
+     * 
+     * This function increments the static variable NBox by 1 and returns its value.
+     * 
+     * @return The new value of NBox.
+     */
+    
+    static int GetNewNBox() {
+        static int NBox = 0;
+        NBox++;
+        return NBox;
+    }
+
+    private:
+    // Método para obtener una nueva instancia de PBOX usando GetMemBox
+    PBOX GetMemBox(const int NDim) {
+        PBOX pB;
+        pB = (PBOX)GetMem((SIZE)1, (SIZE)sizeof(BOX), "GetMemBox");
+        // All generated simplices will be evaluated in F and G.
+        pB->pX.resize(NDim);
+        pB->G.resize(NDim);
+        return pB;
+    }
+    
+};
--- BoxBB-1.1 copy/templates/Generic.cpp
+++ BoxBB-1.1 copy/templates/Generic.cpp
@@ -5,7 +5,7 @@
 template<typename F, typename... Ts>
 class BOX {
     itV F;
-
+    
 public:
     // Virtual function, must be implemented by derived classes
     virtual BOX<F, Ts...> compareTo(const BOX<F, Ts...>& T1, const BOX<F, Ts...>& T2) const = 0;
