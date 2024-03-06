3904341dd78bf36392e2bde4ac88934101a9ff08
Antonio Aranda <antonio.aranda@alboradait.com>
2024-03-06 19:57:48 +0100

    Actualizo obtención de parámetros por consola

--- MyBox/Example-1.0.cpp
+++ MyBox/Example-1.0.cpp
@@ -4,25 +4,51 @@
     begin                : Jan 2020
     copyright            : (C) 2020 by L.G. Casado
     email                : leo@ual.es
- ***************************************************************************/
+@param -d  alfa (alfa is the first  parameter to divide the box)
+
+
+***************************************************************************/
 
 #include "Box.hpp"
-
+#include "./Modules/ParamnsTerminal.cpp"
 /*---------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
+  double alfa;
+  std::map<std::string, double> mapArg;
+
+   //obtener por parámetro el -d que es alfa
+   mapArg = ParamnsTerminal(argc, argv);
+   alfa = mapArg["-d"];
+
+   //Me quedo por aqui haciendo el while, para comparar si el width es menor que alfa
+   
+   /*
+    
  itvV IvV;
  IvV.resize(2);
  IvV[0]=itv(-2., 2.);
  IvV[1]=itv(-3., 3.);
  BOX B(IvV);  //Inicialize a BOX with IvV
  cout << B << endl;  //print the BOX
+ 
  //Init 2 box divider 
  BOX BoXG1(B);
  BOX BoXG2(B);
+ //Aqui necesito pedir los parámetros por consola y que no hacer el bucle hasta que el tamaño no sea menor que el alfa 
+ //alfa es el parámetro -d 
+
+ 
  B.Divide(BoXG1, BoXG2);
+
  cout << BoXG1 << endl;  //print the BOX
  cout << BoXG2 << endl;  //print the BOX
+*/
 return 0;
+
+}
+
+void terminalParamns(){
+
 }
 
---
+++ MyBox/Modules/ParamnsTerminal.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+/***************************************************************************
+                ParamnsTerminal.cpp  -  Template class
+****************************************************************************
+
+/**
+ * Return a map with the parameters of the terminal
+*/
+
+std::map<std::string, double> ParamnsTerminal(int argc, char *argv[])
+{
+    std::map<std::string, double> mapArg;
+    if (argc % 2 == 0)
+    {
+        throw std::invalid_argument("The number of parameters must be even.\n");
+    }
+
+    for (int i = 1; i < argc; i += 2)
+    {
+        // Caso -d
+        if (std::strcmp(argv[i], "-d") == 0)
+        {
+            double valorD = std::stod(argv[i + 1]);
+
+            if (valorD < 0)
+            {
+                // Manejo de error: número de -d debe ser no negativo
+                throw std::invalid_argument("The number of -d must be non-negative.\n");
+            }
+
+            mapArg["-d"] = valorD;
+        }
+    }
+
+    //check if map have -d parameter
+    if (mapArg.find("-d") == mapArg.end())
+    {
+        // Manejo de error: -d es necesario
+        throw std::invalid_argument("Parameter -d is necessary.\n");
+    }
+
+    return mapArg;
+}
+
+/*
+ * This is for test if print correctly the map
+ */
+
+void printMap(const std::map<std::string, int> &mapa)
+{
+    for (const auto &par : mapa)
+    {
+        std::cout << par.first << ": " << par.second << std::endl;
+    }
+}
