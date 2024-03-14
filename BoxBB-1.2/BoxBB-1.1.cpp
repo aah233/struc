d93899a7bb260a369ee6f320951e664a8426bc86
Antonio Aranda <antonio.aranda@alboradait.com>
2024-03-14 19:45:00 +0100

    Actualizo con la generacion de la caja

--- BoxBB-1.2/BoxBB-1.1.cpp
+++ BoxBB-1.2/BoxBB-1.1.cpp
@@ -18,8 +18,10 @@ using namespace std;
 /*---------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
+	int i;
 	double Alpha;  // Termination criterion diam([LB,incumb])<=Alpha.
 	ConstData CtD; // ConstantData. See utils.h
+	std::vector<BOX *> boxTemporales;
 	// numero de veces que se ha mejorado
 
 	// std::cout.precision(17);
@@ -32,6 +34,40 @@ int main(int argc, char *argv[])
 	PrintParams(stderr, CtD, Alpha);   // los imprime para ver que estan bien
 	// Create a new Box with the paramns
 	BOX *pB = new BOX(CtD.InitBox);
+	// while dont have a better aplha
+	while (pB != NULL && (&pB->FX.upper() - &pB->FX.lower() > Alpha)) // alpha parametro de entrada
+	{
+		// Inicializar 2 divisores de caja
+		BOX *pBoXG1 = new BOX(*pB); // Crear un nuevo BOX como copia de B
+		BOX *pBoXG2 = new BOX(*pB); // Crear otro BOX como copia de B
+		std::cout << "Box B1 antes de dividir: " << *pBoXG1 << std::endl;
+		std::cout << "Box B1 antes de dividir: " << *pBoXG2 << std::endl;
+
+		pB->Divides(*pBoXG1, *pBoXG2); // Divide B en BoXG1 y BoXG2
+		std::cout << "Box B1 despues de dividir: " << *pBoXG1 << std::endl;
+		std::cout << "Box B1 despues de dividir: " << *pBoXG2 << std::endl;
+		boxTemporales.push_back(pBoXG1); // No olvides liberar el último B si ya no es necesarioAlmacenar punteros en el vector
+		boxTemporales.push_back(pBoXG2);
+		pBoXG1 = nullptr;
+		pBoXG2 = nullptr;
+		delete pB; // Destruir B explícitamente
+		pB = nullptr;
+		pB = boxTemporales[boxTemporales.size() - 1];
+		boxTemporales.pop_back(); // Liberar el último B creado
+		i++;
+	}
+
+	BOX *pBTemp;
+	// Recorrer todos los elementos y borrar su contenido
+	for (size_t i = 0; i < boxTemporales.size(); i++)
+	{
+		pBTemp = boxTemporales[i];
+		delete pBTemp;
+	}
+	boxTemporales.clear();
+
+	// Liberar el ultimo B creado
+	delete pB;
 
 	return 0;
 }
